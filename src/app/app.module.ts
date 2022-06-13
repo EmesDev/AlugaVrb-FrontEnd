c8dfd5b76a1d4f891f53dbb4ad09ce1e91c70ca8
EMERSON HENRIQUES ANDRADE MENDONCA FRANCO <contato.emersonfranco@gmail.com>
2022-06-13 13:15:29 -0300

    componetes do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { LoginComponent } from './component/login/login.component';
+import { LogoutComponent } from './component/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProfileComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
