ba2ba73f5209026cf2cd1b57a8a76a646dd0b437
EMERSON HENRIQUES ANDRADE MENDONCA FRANCO <contato.emersonfranco@gmail.com>
2022-06-14 10:57:15 -0300

    organizando pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './component/home-page/home-page.component';
-import { ProfileComponent } from './component/profile/profile.component';
-import { LoginComponent } from './component/login/login.component';
-import { LogoutComponent } from './component/logout/logout.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BackingComponent } from './component/backing/backing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    ProfileComponent,
-    LoginComponent,
-    LogoutComponent,
-    BackingComponent
   ],
   imports: [
     BrowserModule,
